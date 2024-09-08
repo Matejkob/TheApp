f9a6bcec97a975656df4db714b22dc02cdcc8046
Mateusz Bąk <bakmatthew@icloud.com>
2024-09-08 12:15:45 +0200

    Initial Commit

---
+++ TheApp/TheAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheAppApp.swift
+//  TheApp
+//
+//  Created by Mateusz Bąk on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TheAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
